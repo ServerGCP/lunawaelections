c9836ef92a19a1bdd737bca8090326f9eeec5518
ServerGCP <163532456+ServerGCP@users.noreply.github.com>
2024-04-15 23:26:44 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -6,7 +6,6 @@ import pandas as pd
 import requests, io
 
 st.set_page_config(page_title="Voting Data Dashboard", layout="wide")
-st_autorefresh(interval=30000, key='data_refresh')
 column_names = ['Num', 'Name', 'Vaas', 'Votes']
 
 def fetch_data():
@@ -19,36 +18,37 @@ def fetch_data():
         return None
     
 def main():
-        csv_data = fetch_data()
-        if csv_data:
-            data = pd.read_csv(io.StringIO(csv_data), names=column_names, index_col="Num")
-            data["Name"] = data["Name"].str.split(' ').str[0]
-            vaas_groups = data.groupby('Vaas')
-    
-            plots = []
-            for name, group in vaas_groups:
-                fig = go.Figure(data=[
-                    go.Bar(
-                        x=group['Name'], 
-                        y=group['Votes'],
-                        marker=dict(color=px.colors.qualitative.Plotly)
-                    )
-                ])
-                fig.update_layout(
-                    title=f"Votes for {name}",
-                    xaxis_title="Member Name",
-                    yaxis_title="Votes",
-                    xaxis_tickangle=-45,
-                    yaxis=dict(dtick=1),
-                    height=300,
-                    width=300
+    csv_data = fetch_data()
+    if csv_data:
+        data = pd.read_csv(io.StringIO(csv_data), names=column_names, index_col="Num")
+        data["Name"] = data["Name"].str.split(' ').str[0]
+        vaas_groups = data.groupby('Vaas')
+
+        plots = []
+        for name, group in vaas_groups:
+            fig = go.Figure(data=[
+                go.Bar(
+                    x=group['Name'], 
+                    y=group['Votes'],
+                    marker=dict(color=px.colors.qualitative.Plotly)
                 )
-                plots.append(fig)
-    
-            rows = [st.columns(4) for _ in range(3)]
-            for row, fig in zip(sum(rows, []), plots):
-                with row:
-                    st.plotly_chart(fig, use_container_width=True)
+            ])
+            fig.update_layout(
+                title=f"Votes for {name}",
+                xaxis_title="Member Name",
+                yaxis_title="Votes",
+                xaxis_tickangle=-45,
+                yaxis=dict(dtick=1),
+                height=300,
+                width=300
+            )
+            plots.append(fig)
+
+        rows = [st.columns(4) for _ in range(3)]
+        for row, fig in zip(sum(rows, []), plots):
+            with row:
+                st.plotly_chart(fig, use_container_width=True)
     
 if __name__ == '__main__':
     main()
+    st_autorefresh(interval=30000, key='data_refresh')
